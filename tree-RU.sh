cb9b40abc3f5eeef901e65160e78eea60aec5a2a
lk2322 <lk2322@ya.ru>
2019-12-29 12:26:07 +0500

    Update

--- tree-RU.sh
+++ tree-RU.sh
@@ -33,7 +33,7 @@ new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
 tput cup $lin $((c - 6)); echo С новым годом!
-tput cup $((lin + 1)) $((c - 10)); echo И больше кода в $new_year
+tput cup $((lin + 1)) $((c - 8)); echo И больше кода в $new_year
 let c++
 k=1
 
@@ -57,7 +57,7 @@ while true; do
         column[$k$i]=$co
         color=$(((color+1)%8))
         # Flashing text
-        sh=-2
+        sh=0
         for l in К О Д А
         do
             tput cup $((lin+1)) $((c+sh))
